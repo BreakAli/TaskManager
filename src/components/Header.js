e1e05acc8b4daa281fdad5fe324ca50e2bf4a865
BreakAli <adarawsha1@gmail.com>
2022-02-05 19:59:18 -0600

    Front + Backend Functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
+import {useLocation} from 'react-router-dom'
 
-
-const Header = ({ title }) => {
-    const counter = 3
-    const onClick = () => {
-        console.log(counter+ 1)
-    }
+const Header = ({ title, onAdd, showAdd}) => {
+    
+    const location = useLocation()
+    
     
   return (
       <header className='header'>
-      <h1>{title}</h1>
-      <Button color='green' text='Add' onClick = {onClick} />
+      <h1>{title}</h1> 
+      {location.pathname =='/' && <Button 
+      color={showAdd ? 'red' : 'green'} 
+      text={showAdd ? 'Close' : 'Add'} 
+      onClick = {onAdd} />
+    }
       
   </header>
   )
