8319c2d87ff7cd1cb7390f3d301392b840ce4e92
BreakAli <adarawsha1@gmail.com>
2022-02-06 00:24:31 -0600

    All Functionality Completed

--- src/App.js
+++ src/App.js
@@ -93,11 +93,10 @@ const App = () => {
       )
   }
   return (
+
     <Router>
     <div className="container">
-      <Header 
-        onAdd ={() => setShowAddTask(!showAddTask)}
-        showAdd ={showAddTask}
+      <Header onAdd ={() => setShowAddTask(!showAddTask)} showAdd ={showAddTask}
       />
       
       <Routes>
